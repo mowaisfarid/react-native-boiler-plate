012ac4b7795d2c71cbea685f3a46823ebb16211b
mowaisfarid <mowaisfarid@gmail.com>
2024-10-05 01:47:38 -0700

    project init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello from the Notifiles</Text>
+    </View>
+  );
+};
+
+export default App;
